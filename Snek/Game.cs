12773ba200eb15a2da6b9e21e0effec515581820
agerard57 <github@agerard.dev>
2022-10-12 16:43:44 +0200

    Add end sentences and enforce number of players
    Now between 2 and 4

--- Snek/Game.cs
+++ Snek/Game.cs
@@ -9,33 +9,68 @@ namespace Snek
     public class Game
     {
         public List<Player> Players { get; set; }
+        public string NumberOfPlayers { get; set; }
         public int CurrentPlayer { get; set; }
+        public bool GameInProgress { get; set; }
 
         public Game()
         {
             Players = new List<Player>();
             CurrentPlayer = 0;
+            GameInProgress = false;
         }
 
         public void CreateGame()
         {
-            Players.Add(new Player("Player 1"));
-            Players.Add(new Player("Player 2"));
+            Console.WriteLine("Welcome to Snek!");
+            NumberOfPlayers = NumberOfPlayersInput();
 
-            while (true)
+            // User input must be a valid number, must also be between 2 and 4 players
+            while (!int.TryParse(NumberOfPlayers, out int n) || n < 2 || n > 4)
+                NumberOfPlayers = NumberOfPlayersInput();
+
+            for (int i = 0; i < int.Parse(NumberOfPlayers); i++)
             {
-                foreach (Player player in Players)
+                Console.WriteLine($"Player {i + 1} - What is your name?");
+                string name = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(name))
+                    name = $"Player {i + 1}";
+
+                Players.Add(new Player(name));
+            }
+            GameInProgress = true;
+
+            while (GameInProgress)
+                GameLoop();
+        }
+
+        private string NumberOfPlayersInput()
+        {
+            Console.WriteLine("Please enter the number of players (between 2 and 4)");
+            return Console.ReadLine();
+        }
+
+        private void GameLoop()
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine($"{player.Name} - Press enter to roll the dice :)");
+                Console.ReadLine();
+                int dice = Dice.Roll();
+                Console.WriteLine($"You rolled a {dice}");
+
+                if (player.Score + dice == 30)
+                    Console.WriteLine("Too bad, 30 makes you loose a turn :(");
+                else
                 {
-                    Console.WriteLine($"{player.Name} - Press enter to roll the dice :)");
-                    Console.ReadLine();
-                    int dice = Dice.Roll();
-                    Console.WriteLine($"You rolled a {dice}");
                     player.Score += dice;
                     Console.WriteLine($"Your score is {player.Score}");
 
                     if (player.Score == 50)
                     {
-                        Console.WriteLine("You won!");
+                        GameInProgress = false;
+                        EndGame();
                         return;
                     }
                     else if (player.Score > 50)
@@ -45,6 +80,15 @@ namespace Snek
                     }
                 }
             }
+        }
+
+        private void EndGame()
+        {
+            Console.WriteLine("Game over!");
+            Console.WriteLine("The winner is:");
+            Console.WriteLine(Players.OrderByDescending(p => p.Score).First().Name);
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
         }
     }
 }
